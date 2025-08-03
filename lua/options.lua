2f39c0b7ad8796a7129185286c3ec539318b2c33
KoiFish0 <koltonweppler@gmail.com>
2025-08-03 16:09:32 -0400

    Changed comments

--- lua/options.lua
+++ lua/options.lua
@@ -1,31 +1,46 @@
--- Hint: use `:h <option>` to figure out the meaning if needed
 vim.g.mapleader = " "
 vim.opt.clipboard = 'unnamedplus'   -- use system clipboard 
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
-vim.opt.mouse = ''                  -- disable mouse. 'n' enables it for normal mode and so on 
+vim.opt.mouse = '' -- Who the hell needs a mouse
 
 -- Tab
-vim.opt.tabstop = 2                 -- number of visual spaces per TAB
-vim.opt.softtabstop = 2             -- number of spacesin tab when editing
-vim.opt.shiftwidth = 2              -- insert 4 spaces on a tab
-vim.opt.expandtab = true            -- tabs are spaces, mainly because of python
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
 -- UI config
-vim.opt.number = true               -- show absolute number
-vim.opt.relativenumber = true       -- add numbers to each line on the left side
-vim.opt.splitbelow = true           -- open new vertical split bottom
-vim.opt.splitright = true           -- open new horizontal splits right
-vim.opt.termguicolors = true        -- enable 24-bit RGB color in the TUI
-vim.opt.showmode = true             -- shows "-- insert --" hints  
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+vim.opt.showmode = true
 vim.opt.guicursor = ""
 
 -- Searching
-vim.opt.incsearch = true            -- search as characters are entered
-vim.opt.hlsearch = false            -- do not highlight matches
-vim.opt.ignorecase = true           -- ignore case in searches by default
-vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Netrw
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 100,
+        })
+    end,
+})
